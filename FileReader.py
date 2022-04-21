e0a1afa44d1f764610e33475b6a72b221ac1760e
AreLudolf <aresb@protonmail.com>
2022-04-21 19:26:41 +0200

    Første commit. Filereader

--- FileReader.py
+++ FileReader.py
@@ -2,6 +2,6 @@
 with open("eksempel_input.txt", "r", encoding="utf-8") as file:
     line_list = []
     for line in file:
-        line_list = [x.strip() for x in line.split(",")]
+        line_list = [entry.strip() for entry in line.split(",")]
         print(line_list)
 
