8523cf3bbc5af5375217fad8d7679c9fbbeb3543
AreLudolf <aresb@protonmail.com>
2022-04-24 19:42:39 +0200

    Database insert, select og print fra database

--- FileReader.py
+++ FileReader.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 
-with open("eksempel_input.txt", "r", encoding="utf-8") as file:
-    line_list = []
-    for line in file:
-        line_list = [entry.strip() for entry in line.split(",")]
-        print(line_list)
-
+def fileread():
+    with open("eksempel_input.txt", "r", encoding="utf-8") as file:
+        line_list = []
+        for line in file:
+            line_list = [entry.strip() for entry in line.split(",")]
+            return(line_list)
--- db_write.py
+++ db_write.py
@@ -2,13 +2,17 @@
 
 import sqlite3
 import datetime
+import FileReader
 
 db_name = datetime.datetime.now()
 
 print(db_name.strftime("%d%m%Y_%H-%M-%S"))
 conn = sqlite3.connect(db_name.strftime("%d%m%Y_%H-%M-%S")+".db")
+print("Opened database successfully")
 
-conn.execute('''CREATE TABLE COMPANY
+FileReader.fileread()
+
+conn.execute('''CREATE TABLE ALARM
          (ID INT PRIMARY KEY     NOT NULL,
          NAME           TEXT    NOT NULL,
          AGE            INT     NOT NULL,
@@ -16,8 +20,17 @@ conn.execute('''CREATE TABLE COMPANY
          SALARY         REAL);''')
 print("Table created successfully")
 
-print("Opened database successfully")
+conn.execute('''INSERT INTO ALARM (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00);
+    ''')
+conn.commit()
+cursor = conn.execute("SELECT id, name, address, salary from ALARM")
+
+for row in cursor:
+    print("ID = ", row[0])
+    print("NAME = ", row[1])
+    print("ADDRESS = ", row[2])
+    print("SALARY = ", row[3], "\n")
 
 conn.close()
 print("Closed database")
-
