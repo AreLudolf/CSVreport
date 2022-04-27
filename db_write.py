76c7903ab3424c1ad2b3e1ea4e3b9102f2bfb331
AreLudolf <aresb@protonmail.com>
2022-04-27 20:04:49 +0200

    Skriver relevant og ryddet data til en ny csv-fil

--- db_write.py
+++ db_write.py
@@ -13,21 +13,34 @@ dbname = (dateandtime.strftime("%d%m%Y_%H-%M-%S"))
 conn = sqlite3.connect(dateandtime.strftime(dbname)+".db")
 print("Opened database successfully")
 
+#Oppretter tabell
 cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS ALARM (%s)" % ", ".join(tablerows))
 print("Table created successfully")
 
 
-
+#Leser input-fil og legger alle entries inn i databasen
 with open("eksempel_input.txt", "r", encoding="utf-8") as file:
     line_list = []
     for line in file:
         line_list = [entry.strip() for entry in line.split(",")]
         cursor.execute('''INSERT INTO ALARM (alarmRef) VALUES(?) ''', [line_list[11]])
-        conn.commit()
         for i in range(len(line_list) - 1):
             cursor.execute('''UPDATE ALARM SET (%s) = (?) WHERE alarmRef == ("%s");''' % (tablerows[i], line_list[11]), [line_list[i]])
 print("Entries added to database")
+
 conn.commit()
+
+#hente ut fil uten canceled status med mindre det er tilstede og borte
+cursor.execute('''SELECT date, time, alarmName, status FROM ALARM WHERE status != "Canceled" OR alarmGroup = "Empty group"''')
+data = cursor.fetchall()
+print(data)
+file1 = open("output_" + dbname + ".csv", "a")
+for row in data:
+    file1.write(', '.join(row) + '\n')
+
+
+file1.close()
+
 cursor.close()
 print("Closed database")
