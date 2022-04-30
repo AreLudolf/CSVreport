6e08b1d774504fd269a15ec2ffc807254480137a
AreLudolf <aresb@protonmail.com>
2022-04-30 14:18:06 +0200

    Henter metadata fra input-fil

--- db_write.py
+++ db_write.py
@@ -5,10 +5,8 @@ import datetime
 import sys
 
 file_name = sys.argv[1]
-
+#file_name = "ex_meta.csv"
 # hentes fra metadata på sikt
-tablerows = ['date', 'time', 'server', 'alarmID', 'alarmName', 'launchedBy',
-             'status', 'alarmGroup', 'txtMsg', 'dspMsg1', 'dspMsg2', 'alarmRef']
 
 # Filnavn = dato + klokkeslett
 dateandtime = datetime.datetime.now()
@@ -16,9 +14,14 @@ dbname = (dateandtime.strftime(file_name + "_%d%m%Y_%H-%M-%S"))
 conn = sqlite3.connect(dateandtime.strftime(dbname)+".db")
 print("Opened database successfully")
 
+#lager metadata av første linje i fila
+with open(file_name) as f:
+    firstline = f.readlines()[0].rstrip()
+    meta = [entry.strip().replace(" ", "_") for entry in firstline.split(",")]
+
 # Oppretter tabell
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS ALARM (%s)" % ", ".join(tablerows))
+cursor.execute("CREATE TABLE IF NOT EXISTS ALARM (%s)" % ", ".join(meta))
 print("Table created successfully")
 
 
@@ -27,23 +30,23 @@ with open(file_name, "r", encoding="utf-8") as file:
     line_list = []
     for line in file:
         line_list = [entry.strip() for entry in line.split(",")]
-        cursor.execute('''INSERT INTO ALARM (alarmRef) VALUES(?) ''', [line_list[11]])
-        for i in range(len(line_list) - 1):
+        cursor.execute('''INSERT INTO ALARM (Alarm_Reference) VALUES(?) ''', [line_list[11]])
+        for i in range(len(line_list)):
             cursor.execute('''UPDATE ALARM SET (%s) = (?) 
-            WHERE alarmRef == ("%s");''' % (tablerows[i], line_list[11]), [line_list[i]])
+            WHERE Alarm_Reference == ("%s");''' % (meta[i], line_list[11]), [line_list[i]])
 print("Entries added to database")
 
 conn.commit()
 
 # hente ut fil uten canceled status med mindre det er tilstede og borte
-cursor.execute('''SELECT date, time, alarmName, status FROM ALARM 
-WHERE status != "Canceled" OR alarmGroup = "Empty group"''')
+cursor.execute('''SELECT Date, Time, Alarm_Name, Alarm_Status FROM ALARM 
+WHERE Alarm_Status != "Canceled" OR Alarm_Group = "Empty group"''')
 
 queryResult = cursor.fetchall()
 outputFile = open(dbname + "_output.csv", "a")
 for row in queryResult:
     outputFile.write(', '.join(row) + '\n')
-print("Written to file: " + dbname + "_output.csv")
+print("Cleaned up csv written to file: " + dbname + "_output.csv")
 outputFile.close()
 
 
