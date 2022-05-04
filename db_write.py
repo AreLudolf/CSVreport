d5929c94f9c0f550c0369eaba20fa8426dc9c73b
AreLudolf <aresb@protonmail.com>
2022-05-04 18:17:21 +0200

    Responstid funker

--- db_write.py
+++ db_write.py
@@ -4,8 +4,8 @@ import sqlite3
 import datetime
 import sys
 
-file_name = sys.argv[1]
-#file_name = "ex_meta.csv"
+#file_name = sys.argv[1]
+file_name = "ex_meta.csv"
 
 # Filnavn = dato + klokkeslett
 dateandtime = datetime.datetime.now()
@@ -47,6 +47,47 @@ for row in queryResult:
 print("Cleaned up csv written to file: " + dbname + "_output.csv")
 outputFile.close()
 
+
+#regne ut responstid:
+cursor.execute('''SELECT Date, Time, Alarm_Name FROM ALARM
+WHERE Alarm_Status != "Canceled" OR Alarm_Group = "Empty group"''')
+responsQuery = cursor.fetchall()
+responsQuery.reverse()
+active_alarm = [("Date", "Time", "Alarm Name")]
+for row in responsQuery:
+    alarmType = row[2].split(" ")
+    alarmName = row[2]
+    tilstedeBorte = ["Tilstede", "Borte"]
+    entryExist = False
+    if alarmType[0] not in tilstedeBorte:
+        for i in active_alarm:
+            if alarmName in i[2]:
+                entryExist = True
+
+        if entryExist == False:
+            active_alarm.append(row)
+
+        if entryExist == True:
+            entryExist = False
+
+
+    if alarmType[0] == "Tilstede":
+        tilstedeTid = (row[0], row[1])
+        tilstedeTidStr = datetime.datetime.strptime(' '.join(tilstedeTid), "%d.%m.%Y %H:%M:%S")
+
+        for i in active_alarm:
+            roomNr = i[2].split(" ")
+            if alarmType[1] in roomNr:
+                alarmTime = (i[0], i[1])
+                alarmTimeStr = datetime.datetime.strptime(' '.join(alarmTime), "%d.%m.%Y %H:%M:%S")
+                tdelta = tilstedeTidStr - alarmTimeStr
+                active_alarm_type = i[2]
+                print("Responstid på alarm", active_alarm_type, ": ", tdelta)
+                active_alarm.remove(i)
+
+
+
+print(active_alarm)
 
 cursor.close()
 print("Closed database")
