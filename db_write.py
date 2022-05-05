ed531b7ddb9626027e341c65e176c981affe0aef
are.breidablikk <aresb@protonmail.com>
2022-05-05 15:59:47 +0200

    Pokkers GUI med testframez

--- db_write.py
+++ db_write.py
@@ -2,10 +2,10 @@
 
 import sqlite3
 import datetime
-import sys
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
+from tkinter import scrolledtext as st
 
 cursor = ''
 dbname = ''
@@ -14,8 +14,8 @@ labeltext = ''
 
 
 def read_to_db():
-#file_name = sys.argv[1]
     global file_name
+    global dbname
 # Filnavn = dato + klokkeslett
     dateandtime = datetime.datetime.now()
     dbname = (dateandtime.strftime(file_name + "_%d%m%Y_%H-%M-%S"))
@@ -46,6 +46,7 @@ def read_to_db():
 
     conn.commit()
 
+
 # hente ut fil uten canceled status med mindre det er tilstede og borte
 def clean_to_csv():
     global cursor
@@ -57,6 +58,7 @@ def clean_to_csv():
     for row in queryResult:
         outputFile.write(', '.join(row) + '\n')
         print(', '.join(row) + '\n')
+        text_area.insert(tk.INSERT, row)
     print("Cleaned up csv written to file: " + dbname + "_output.csv")
     outputFile.close()
 
@@ -84,7 +86,6 @@ def response_time():
             if entryExist == True:
                 entryExist = False
 
-
         if alarmType[0] == "Tilstede":
             tilstedeTid = (row[0], row[1])
             tilstedeTidStr = datetime.datetime.strptime(' '.join(tilstedeTid), "%d.%m.%Y %H:%M:%S")
@@ -102,23 +103,10 @@ def response_time():
     print(active_alarm)
 
 
-root = tk.Tk()
-root.title('CSV-rapportfikser')
-
-window_width = 500
-window_height = 300
-
-# get the screen dimension
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-# find the center point
-center_x = int(screen_width/2 - window_width / 2)
-center_y = int(screen_height/2 - window_height / 2)
-
-# set the position of the window to the center of the screen
-root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-
+def write_to_scrolled():
+    with open(dbname + "_output.csv", "r", encoding="utf-8") as file:
+        for line in file:
+            text_area.insert(tk.INSERT, line)
 
 
 def select_file():
@@ -138,20 +126,38 @@ def select_file():
     label.pack()
     print(file_name)
 
-# open button
+
+'''
+-----------------------------------GUI----------------------------------
+'''
+root = tk.Tk()
+root.title('CSV-rapportfikser')
+
+window_width = 900
+window_height = 300
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
 open_button = ttk.Button(
     root,
     text='Open a File',
     command=select_file
 )
-open_button.pack(expand=True)
+
+open_button.pack(ipadx=0, ipady=0, )
 
 submit_button = ttk.Button(
     root,
     text='Submit',
     command=read_to_db
 )
-submit_button.pack()
+
+submit_button.pack(ipadx=1, ipady=0)
 
 hitit_button = ttk.Button(
     root,
@@ -161,3 +167,57 @@ hitit_button = ttk.Button(
 hitit_button.pack()
 
 root.mainloop()
+test = tk.Tk()
+window_width = 900
+window_height = 300
+screen_width = test.winfo_screenwidth()
+screen_height = test.winfo_screenheight()
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+test.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+test.resizable(height=False, width=False)
+treeview_frame = tk.Frame(test, background="#FFF0C1", bd=1)
+graph_frame = tk.Frame(test, background="#D2E2FB", bd=1, relief="sunken")
+text_frame = tk.Frame(test, background="#CCE4CA", bd=1, relief="sunken")
+button_frame = tk.Frame(test, background="#F5C2C1", bd=1, relief="sunken")
+
+treeview_frame.grid(row=0, column=0, sticky="nsew", padx=1, pady=1)
+graph_frame.grid(row=1, column=0, sticky="nsew", padx=2, pady=2)
+text_frame.grid(row=0, column=1, rowspan=2, sticky="nsew", padx=2, pady=2)
+button_frame.grid(row=0, column=2, rowspan=2, sticky="nsew", padx=2, pady=2)
+
+test.grid_rowconfigure(0, weight=3)
+test.grid_rowconfigure(1, weight=2)
+
+test.grid_columnconfigure(0, weight=3)
+test.grid_columnconfigure(1, weight=2)
+test.grid_columnconfigure(2, weight=2)
+
+sb = ttk.Scrollbar(text_frame)
+sb.pack(side="right", fill="y")
+
+open_button = ttk.Button(
+    treeview_frame,
+    text='Open a File',
+    command=select_file
+)
+
+open_button.pack()
+
+submit_button = ttk.Button(
+    treeview_frame,
+    text='Submit',
+    command=read_to_db
+)
+
+submit_button.pack()
+
+hitit_button = ttk.Button(
+    treeview_frame,
+    text='Hit it!',
+    command=clean_to_csv
+)
+hitit_button.pack()
+test.mainloop()
