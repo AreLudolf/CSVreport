5372378444db69f03a8b8c82b8b562aeb6d23196
are.breidablikk <aresb@protonmail.com>
2022-05-12 13:08:14 +0200

    Ryddet i kode. Responstid til xlsx funker ikke

--- CSVreport.py
+++ CSVreport.py
@@ -2,147 +2,158 @@
 
 import sqlite3
 import datetime
-import tkinter as tk
-from tkinter import ttk
 import pandas as pd
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox as msg
 from pandastable import Table
+import os
 
 cursor = ''
-conn=''
+conn = ''
 dbname = ''
 file_name = ''
 labeltext = ''
 dateandtime = ''
 meta = ''
 cleanup_query = '''SELECT Date, Time, Alarm_Name, Alarm_Status FROM ALARM
-	WHERE Alarm_Status != "Canceled" OR Alarm_Group = "Empty group"'''
+WHERE Alarm_Status != "Canceled" OR Alarm_Group = "Empty group"'''
 
 response_query = '''SELECT Date, Time, Alarm_Name FROM ALARM
-	WHERE Alarm_Status != "Canceled" OR Alarm_Group = "Empty group"'''
+WHERE Alarm_Status != "Canceled" OR Alarm_Group = "Empty group"'''
+
 
 def create_filename():
-	global file_name
-	global dbname
-	global dateandtime
-	dateandtime = datetime.datetime.now()
-	dbname = (dateandtime.strftime(file_name + "_%d%m%Y_%H-%M-%S"))
-	print('create_filename: file_name created')
+    global file_name
+    global dbname
+    global dateandtime
+    dateandtime = datetime.datetime.now()
+    dbname = (dateandtime.strftime(file_name + "_%d%m%Y_%H-%M-%S"))
+    print('create_filename: file_name created')
+
 
 def create_database():
-	global dateandtime
-	global meta
-	global conn
-	conn = sqlite3.connect(dateandtime.strftime(dbname)+".db")
-	print("create_database: Opened database successfully")
+    global dateandtime
+    global meta
+    global conn
+    conn = sqlite3.connect(dbname + ".db")
+    print("create_database: Opened database successfully")
 
-	#lager metadata av første linje i fila
-	with open(file_name) as f:
-		firstline = f.readlines()[0].rstrip()
-		meta = [entry.strip().replace(" ", "_") for entry in firstline.split(",")]
-		print('create_database: metadata created successfully')
+    # lager metadata av første linje i fila
+    with open(file_name) as f:
+        firstline = f.readlines()[0].rstrip()
+        meta = [entry.strip().replace(" ", "_") for entry in firstline.split(",")]
+        print('create_database: metadata created successfully')
 
-	global cursor
-	cursor = conn.cursor()
-	cursor.execute("CREATE TABLE IF NOT EXISTS ALARM (%s)" % ", ".join(meta))
-	print("create_database: Table created successfully")
-	conn.commit()
+    global cursor
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS ALARM (%s)" % ", ".join(meta))
+    print("create_database: Table created successfully")
+    conn.commit()
+
 
 def write_to_db():
-	global meta
-	global conn
-	# Leser input-fil og legger alle entries inn i databasen
-	with open(file_name, "r", encoding="utf-8") as file:
-		for line in file:
-			line_list = [entry.strip() for entry in line.split(",")]
-			values = ','.join(['?'] * len(line_list))
-			cursor.execute('''INSERT INTO ALARM ({}) VALUES({}) '''.format(", ".join(meta), values), line_list)
+    global meta
+    global conn
+    # Leser input-fil og legger alle entries inn i databasen
+    with open(file_name, "r", encoding="utf-8") as file:
+        for line in file:
+            line_list = [entry.strip() for entry in line.split(",")]
+            values = ','.join(['?'] * len(line_list))
+            cursor.execute('''INSERT INTO ALARM ({}) VALUES({}) '''.format(", ".join(meta), values), line_list)
 
-	print("write_to_db: Entries added to database")
-	conn.commit()
+    print("write_to_db: Entries added to database")
+    conn.commit()
+
 
 def write_to_xl(file_name):
-	df = pd.read_csv(file_name)
-	if (len(df) == 0):
-		msg.showinfo('No Rows Selected', 'CSV has no rows')
-	else:
+    df = pd.read_csv(file_name)
+    print('write_to_xl() file_name: ', file_name)
+    if (len(df) == 0):
+        msg.showinfo('No Rows Selected', 'CSV has no rows')
+    else:
 
-		# saves in the current directory
-		with pd.ExcelWriter('{}.xlsx'.format(file_name)) as writer:
-			df.to_excel(writer, 'Alarm')
-			writer.save()
-			msg.showinfo('Excel file ceated', 'Excel File created and database created')
-			print('Excel file ceated', 'Excel File created and database created')
+        # saves in the current directory
+        with pd.ExcelWriter('{}.xlsx'.format(file_name)) as writer:
+            df.to_excel(writer, 'Alarm')
+            writer.save()
+            msg.showinfo('Excel file ceated', 'Excel File created: {}.xlsx'.format(file_name))
+            print('Excel File created: {}.xlsx'.format(file_name))
 
 
 def clean_csv():
-	global cursor
-	cursor.execute(cleanup_query)
-	queryResult = cursor.fetchall()
-	print('Clean_csv, DB queried successfully')
-	#skriver til fil
-	print('Clean_csv: opening file')
-	outputFile = open(dbname + "_output.csv", "a")
-	for row in queryResult:
-		outputFile.write(', '.join(row) + '\n')
-		print(', '.join(row) + '\n')
-	print("Cleaned up csv written to file: " + dbname + "_output.csv")
-	
-	write_to_xl(dbname + "_output.csv")
-#DELETE DBNAME + OUTPUT.CSV
-	outputFile.close()
+    global cursor
+    cursor.execute(cleanup_query)
+    query_result = cursor.fetchall()
+    print('Clean_csv, DB queried successfully')
+    # skriver til fil
+    print('Clean_csv: opening file')
+    output_file = open(dbname + "_output.csv", "a")
+    for row in query_result:
+        output_file.write(', '.join(row) + '\n')
+    print("Cleaned up csv written to file: " + dbname + "_output.csv")
 
-	
+    output_file.close()
+
+def show_table(CsvToExcel, df):
+    CsvToExcel.f2 = Frame(CsvToExcel.root, height=200, width=300)
+    CsvToExcel.f2.pack(fill=BOTH, expand=1)
+    CsvToExcel.table = Table(CsvToExcel.f2, dataframe=df, read_only=True)
+    CsvToExcel.table.show()
+
 def response_time():
-	global file_name
-	global dbname
-	cursor.execute(response_query)
-	responsQuery = cursor.fetchall()
-	responsQuery.reverse()
-	active_alarm = [("Date", "Time", "Alarm Name")]
-	response_file = open(dbname + '_responstid.csv', 'w')
-	for row in responsQuery:
-		alarmType = row[2].split(" ")
-		alarmName = row[2]
-		tilstedeBorte = ["Tilstede", "Borte"]
-		entryExist = False
+    global file_name
+    global dbname
+    cursor.execute(response_query)
+    query_result = cursor.fetchall()
+    query_result.reverse()
+    active_alarm = [("Date", "Time", "Alarm Name")]
+    response_file = open(dbname + '_responstid.csv', 'w')
+    for row in query_result:
+        alarmType = row[2].split(" ")
+        alarmName = row[2]
+        tilstede_borte = ["Tilstede", "Borte"]
+        entry_exist = False
 
-		if alarmType[0] not in tilstedeBorte:
-			for i in active_alarm:
-				if alarmName in i[2]:
-					entryExist = True
+        if alarmType[0] not in tilstede_borte:
+            for i in active_alarm:
+                if alarmName in i[2]:
+                    entry_exist = True
 
-			if entryExist == False:
-				active_alarm.append(row)
+            if entry_exist == False:
+                active_alarm.append(row)
 
-			if entryExist == True:
-				entryExist = False
+            if entry_exist == True:
+                entry_exist = False
 
-		if alarmType[0] == "Tilstede":
-			tilstedeTid = (row[0], row[1])
-			tilstedeTidStr = datetime.datetime.strptime(' '.join(tilstedeTid), "%d.%m.%Y %H:%M:%S")
+        if alarmType[0] == "Tilstede":
+            tilstede_tid = (row[0], row[1])
+            tilstede_tid_str = datetime.datetime.strptime(' '.join(tilstede_tid), "%d.%m.%Y %H:%M:%S")
 
-			for i in active_alarm:
-				roomNr = i[2].split(" ")
-				if alarmType[1] in roomNr:
-					alarmTime = (i[0], i[1])
-					alarmTimeStr = datetime.datetime.strptime(' '.join(alarmTime), "%d.%m.%Y %H:%M:%S")
-					tdelta = tilstedeTidStr - alarmTimeStr
-					write_response = ', '.join(i + (str(tdelta),))
-					print(write_response)
-					response_file.write(write_response + '\n')
-					active_alarm.remove(i)
-	response_file.close()
-	write_to_xl(response_file)
-#DELETE RESPONSE_FILE
+            for i in active_alarm:
+                roomNr = i[2].split(" ")
+                if alarmType[1] in roomNr:
+                    alarmTime = (i[0], i[1])
+                    alarm_time_str = datetime.datetime.strptime(' '.join(alarmTime), "%d.%m.%Y %H:%M:%S")
+                    tdelta = tilstede_tid_str - alarm_time_str
+                    write_response = ', '.join(i + (str(tdelta),))
+                    print(write_response)
+                    response_file.write(write_response + '\n')
+                    active_alarm.remove(i)
+    response_file.close()
+    print('RESPONSE_FILE: ', response_file)
+    write_to_xl(response_file)
+
+
+# DELETE RESPONSE_FILE
 
 
 '''
 ---------------------------GUI------------------------
 '''
-class csv_to_excel:
+
+
+class CsvToExcel:
 
     def __init__(self, root):
 
@@ -160,16 +171,17 @@ class csv_to_excel:
                                    text='CSV cleanup',
                                    fg='Black')
         self.message_label2 = Label(self.f,
-                                    text='Det spretter opp en melding når den er ferdig å lese filene. Vær tålmodig det kan ta litt tid',
+                                    text='Det spretter opp en melding når den er ferdig å lese filene. '
+                                         'Vær tålmodig det kan ta litt tid',
                                     fg='Red')
 
         # Buttons
         self.convert_button = Button(self.f,
                                      text='Åpne',
-                                     command=self.convert_csv_to_xls)
+                                     command=self.open_file)
         self.display_button = Button(self.f,
                                      text='Cleanup',
-                                     command=self.display_xls_file)
+                                     command=self.clean_up_file)
         self.exit_button = Button(self.f,
                                   text='Responstid',
                                   command=response_time)
@@ -184,7 +196,9 @@ class csv_to_excel:
         self.exit_button.grid(row=3, column=2,
                               padx=10, pady=15)
 
-    def convert_csv_to_xls(self):
+
+
+    def open_file(self):
         try:
             self.file_name = filedialog.askopenfilename(initialdir='/Desktop',
                                                         title='Select a CSV file',
@@ -198,45 +212,39 @@ class csv_to_excel:
             create_filename()
             create_database()
             write_to_db()
-            if (len(df) == 0):
+            if len(df) == 0:
                 msg.showinfo('No Rows Selected', 'CSV has no rows')
             else:
-                write_to_xl(file_name)
-
-            self.f2 = Frame(self.root, height=200, width=300)
-            self.f2.pack(fill=BOTH, expand=1)
-            self.table = Table(self.f2, dataframe=df, read_only=True)
-            self.table.show()
+                print('open_file(): File opened, show table')
+            show_table(self, df)
 
         except FileNotFoundError as e:
             msg.showerror('Error in opening file', e)
 
-
-    def display_xls_file(self):
+    def clean_up_file(self):
         self.table.close()
         clean_csv()
+        print('clean_up_file(): clean_csv() kjørt')
         try:
             csv_file_name = dbname + "_output.csv"
             df = pd.read_csv(csv_file_name)
-            if (len(df) == 0):
+            if len(df) == 0:
                 msg.showinfo('No Rows Selected', 'CSV has no rows')
             else:
 
                 # saves in the current directory
                 write_to_xl(csv_file_name)
-
+            os.rename('{}.xlsx'.format(csv_file_name), '{}_output.xlsx'.format(dbname))
+            print('Renamed file: {}.xlsx'.format(csv_file_name), 'to: {}_output.xlsx'.format(dbname))
             self.file_name = dbname + '_output.xlsx'
             df = pd.read_excel(self.file_name)
 
-            if (len(df) == 0):
+            if len(df) == 0:
                 msg.showinfo('No records', 'No records')
             else:
                 pass
 
-            self.f2 = Frame(self.root, height=200, width=300)
-            self.f2.pack(fill=BOTH, expand=1)
-            self.table = Table(self.f2, dataframe=df, read_only=True)
-            self.table.show()
+            show_table(self, df)
 
         except FileNotFoundError as e:
             print(e)
@@ -246,6 +254,6 @@ class csv_to_excel:
 root = Tk()
 root.title('CSV converter')
 
-obj = csv_to_excel(root)
+obj = CsvToExcel(root)
 root.geometry('800x600')
 root.mainloop()
